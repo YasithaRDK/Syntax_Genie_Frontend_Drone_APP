e9a3d7f8d7cd9121e77b344e470ca51ee21d1227
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-20 00:50:56 +0530

    get available data from db and show it in available drone page

--- src/components/drone/DroneList.jsx
+++ src/components/drone/DroneList.jsx
@@ -1,22 +1,32 @@
-import { Card, CardBody, CardHeader, CardText } from "reactstrap";
+import { Link } from "react-router-dom";
+import { Button, Card, CardBody, CardHeader, CardText } from "reactstrap";
 
-const DroneList = () => {
+const DroneList = (drone) => {
+  const dotColor = drone.drone.state === "IDLE" ? "green" : "#d6d30e";
   return (
     <Card
       className="m-2"
-      color="success"
-      inverse
       style={{
-        width: "25rem",
+        width: "22rem",
       }}
     >
-      <CardHeader>12458654</CardHeader>
+      <CardHeader className="d-flex justify-content-between align-items-center">
+        {drone.drone._id}{" "}
+        <span style={{ color: dotColor }} className="fs-3">
+          ●
+        </span>
+      </CardHeader>
       <CardBody>
-        <CardText>Model: Lightweight</CardText>
-        <CardText>Weight Limit: 500</CardText>
-        <CardText>Battery: 50% </CardText>
-        <CardText>State: IDLE</CardText>
+        <CardText>Model: {drone.drone.model}</CardText>
+        <CardText>Weight Limit: {drone.drone.weightLimit}</CardText>
+        <CardText>Battery: {drone.drone.batteryCapacity}% </CardText>
+        <CardText>State: {drone.drone.state}</CardText>
       </CardBody>
+      <Link to={`/load-medication/${drone.drone._id}`} className="m-2">
+        <Button color="primary" className="w-100">
+          Load Medication
+        </Button>
+      </Link>
     </Card>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,16 @@
 import { Container } from "reactstrap";
 import DroneList from "../components/drone/DroneList";
+import { useGetAvailableDronesQuery } from "../features/droneSlice";
 
 const Home = () => {
+  const { data: drones } = useGetAvailableDronesQuery();
+
   return (
     <Container>
       <h3 className="text-center mt-4">Available Drones List</h3>
       <div className="d-flex flex-wrap my-3">
-        <DroneList />
-        <DroneList />
-        <DroneList />
-        <DroneList />
-        <DroneList />
-        <DroneList />
+        {drones &&
+          drones.map((drone, index) => <DroneList key={index} drone={drone} />)}
       </div>
     </Container>
   );
