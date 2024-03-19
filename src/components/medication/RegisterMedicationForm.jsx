d292cfe1f98b2d0180a68e1569f1f5d5c018cbc8
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-20 03:07:35 +0530

    add loading effect

--- src/components/medication/RegisterMedicationForm.jsx
+++ src/components/medication/RegisterMedicationForm.jsx
@@ -11,6 +11,7 @@ import {
 } from "reactstrap";
 import { useRegisterMedicationMutation } from "../../features/medicationSlice";
 import { toast } from "react-toastify";
+import Loader from "../Loader";
 
 const RegisterMedicationForm = () => {
   const [formData, setFormData] = useState({
@@ -43,7 +44,7 @@ const RegisterMedicationForm = () => {
     data.append("image", selectedImage);
     try {
       await RegisterMedication(data).unwrap();
-      toast.success("Drone successfully registered...!");
+      toast.success("Medication successfully registered...!");
       setFormData({
         name: "",
         weight: "",
@@ -51,7 +52,7 @@ const RegisterMedicationForm = () => {
       });
       setSelectedImage(null);
     } catch (error) {
-      console.error("Error:", error);
+      toast.error(error.data.message);
     }
   };
 
@@ -60,59 +61,63 @@ const RegisterMedicationForm = () => {
       <CardHeader>
         <h3 className="text-center">Medication Registration Form</h3>
       </CardHeader>
-      <CardBody>
-        <div className="d-flex justify-content-center">
-          <Form onSubmit={handleSubmit} className="w-75">
-            <FormGroup>
-              <Label for="name">Name:</Label>
-              <Input
-                type="text"
-                name="name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="weight">Weight:</Label>
-              <Input
-                type="number"
-                name="weight"
-                id="weight"
-                value={formData.weight}
-                onChange={handleChange}
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="code">Code:</Label>
-              <Input
-                type="text"
-                name="code"
-                id="code"
-                value={formData.code}
-                onChange={handleChange}
-                required
-              />
-            </FormGroup>
-            <FormGroup>
-              <Label for="image">Image:</Label>
-              <Input
-                type="file"
-                name="image"
-                id="image"
-                value={formData.image}
-                onChange={handleImageChange}
-                required
-              />
-            </FormGroup>
-            <Button type="submit" color="primary">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      </CardBody>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <CardBody>
+          <div className="d-flex justify-content-center">
+            <Form onSubmit={handleSubmit} className="w-75">
+              <FormGroup>
+                <Label for="name">Name:</Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="name"
+                  value={formData.name}
+                  onChange={handleChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="weight">Weight:</Label>
+                <Input
+                  type="number"
+                  name="weight"
+                  id="weight"
+                  value={formData.weight}
+                  onChange={handleChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="code">Code:</Label>
+                <Input
+                  type="text"
+                  name="code"
+                  id="code"
+                  value={formData.code}
+                  onChange={handleChange}
+                  required
+                />
+              </FormGroup>
+              <FormGroup>
+                <Label for="image">Image:</Label>
+                <Input
+                  type="file"
+                  name="image"
+                  id="image"
+                  value={formData.image}
+                  onChange={handleImageChange}
+                  required
+                />
+              </FormGroup>
+              <Button type="submit" color="primary">
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </CardBody>
+      )}
     </Card>
   );
 };
